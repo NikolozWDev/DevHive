aae1dfa20e09ffcccc3e97882d4f182c4bbc56f6
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-07-07 00:09:11 +0400

    Django REST Framework / Django Cors Headers

--- firstpro/base/api/urls.py
+++ firstpro/base/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import get_routes
+from .views import get_routes, get_rooms, get_room
 
 urlpatterns = [
-    path('', get_routes)
+    path('', get_routes),
+    path('rooms/', get_rooms),
+    path('rooms/<str:pk>/', get_room)
 ]
